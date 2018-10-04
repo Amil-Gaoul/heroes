538ad8fbe4a7ca222bdaf7b8e16dc950c819b8dc
Andrey <amildream@gmail.com>
2018-10-04 23:01:47 +0700

    Auto-generated commit

---
+++ app/api/all-heroes.ts
@@ -0,0 +1,75 @@
+import { Hero } from './../shared/models/hero.model';
+export const HeroesData: Hero[] = [
+    {
+        id: 1,
+        avatar: 'assets/images/heroes/1-thumbnail.jpg',
+        name: 'Spider-man',
+        alterEgo: 'Peter Parker',
+        synopsis: 'Peter Benjamin Parker was born in Queens to Richard and Mary Parker. While Peter was still an infant, his parents were assigned by the CIA to infiltrate the Algerian based spy ring controlled by the communist agent, the Red Skull. The moment the Red Skull learned that Richard and Mary were double agents, he had them killed by one of his agents, the Finisher, in an airplane crash overseas and then framed them for treason. Before their deaths however, Richard and Mary had another child unknown to all others including Peter himself, a daughter and Peter\'s younger sister, Teresa. An orphaned Peter went to live with his uncle and aunt, Ben and May Parker, in Forest Hills, New York; they became Peter’s surrogate parents and raised him as if he were their own son. Over the next nine years, Parker grew to be extremely bright in science and became a high honors student at Midtown High School. Parker\'s shyness and scholastic interest often made him a social outcast and a target for bullies, primarily football star Flash Thompson.',
+        likes: 1500
+    },
+    {
+        id: 2,
+        avatar: 'assets/images/heroes/2-thumbnail.jpg',
+        name: 'Superman',
+        alterEgo: 'Clark Kent',
+        synopsis: 'Superman was born on the planet Krypton, and as a baby was sent to Earth in a small spaceship by his scientist father Jor-El, moments before Krypton was destroyed in a natural cataclysm. His ship landed in the American countryside, where he was discovered and adopted by a farming couple. They named him Clark Kent and imbued him with a strong moral compass. He developed various superhuman abilities, which he resolved to use for the benefit of humanity. Clark Kent resides in the fictional American city of Metropolis, where he works as a journalist for the Daily Planet, a newspaper. To protect his privacy, he changes into a colorful costume and uses the alias "Superman" when fighting crime. Superman\s love interest is his fellow journalist Lois Lane, and his classic archenemy is the genius inventor Lex Luthor. He is a friend of many other superheroes in the DC Universe, such as Batman and Wonder Woman.',
+        likes: 1500
+    },
+    {
+        id: 3,
+        avatar: 'assets/images/heroes/3-thumbnail.jpg',
+        name: 'Batman',
+        alterEgo: 'Bruce Wayne',
+        synopsis: 'Batman has been Gotham\'s protector for decades, CEO of Wayne Enterprises, Patriarch of the Bat Family and veteran member of the Justice League. is a superhero co-created by artist Bob Kane and writer Bill Finger and published by DC Comics. The character made his first appearance in Detective Comics #27 (May, 1939). Batman is the secret identity of Bruce Wayne. Witnessing the murder of his parents as a child leads him to train himself to physical and intellectual perfection and don a bat-themed costume in order to fight crime. Batman operates in Gotham City, assisted by various supporting characters including his sidekick Robin and his butler Alfred Pennyworth, and fights an assortment of villains influenced by the characters\' roots in film and pulp magazines. Unlike most superheroes, he does not possess any superpowers; he makes use (to the best that he can) of intellect, detective skills, science and technology, wealth, physical prowess, and intimidation in his war on crime.',
+        likes: 1500
+    },
+    {
+        id: 4,
+        avatar: 'assets/images/heroes/4-thumbnail.jpg',
+        name: 'Wonder woman',
+        alterEgo: 'Diana Prince',
+        synopsis: 'Wonder Woman is a fictional superhero appearing in American comic books published by DC Comics. The character is a founding member of the Justice League, and an ambassador of the Amazon people. The character first appeared in All Star Comics #8 in October 1941 with her first feature in Sensation Comics #1, January 1942. The Wonder Woman title has been published by DC Comics almost continuously except for a brief hiatus in 1986. In her homeland, the island nation of Themyscira, her official title is Princess Diana of Themyscira, Daughter of Hippolyta. When blending into the society outside of her homeland, she adopts her civilian identity Diana Prince.',
+        likes: 1500
+    },
+    {
+        id: 5,
+        avatar: 'assets/images/heroes/5-thumbnail.jpg',
+        name: 'Iron man',
+        alterEgo: 'Tony Stark',
+        synopsis: 'A wealthy American business magnate, playboy, and ingenious scientist, Anthony Edward "Tony" Stark suffers a severe chest injury during a kidnapping. When his captors attempt to force him to build a weapon of mass destruction, he instead creates a powered suit of armor to save his life and escape captivity. Later, Stark develops his suit, adding weapons and other technological devices he designed through his company, Stark Industries. He uses the suit and successive versions to protect the world as Iron Man. Although at first concealing his true identity, Stark eventually declared that he was, in fact, Iron Man in a public announcement.',
+        likes: 1500
+    },
+    {
+        id: 6,
+        avatar: 'assets/images/heroes/6-thumbnail.jpg',
+        name: 'Captain America',
+        alterEgo: 'Steven Grant Rogers',
+        synopsis: 'Captain America is a fictional superhero appearing in American comic books published by Marvel Comics. Created by cartoonists Joe Simon and Jack Kirby, the character first appeared in Captain America Comics #1 (cover dated March 1941) from Timely Comics, a predecessor of Marvel Comics. Captain America was designed as a patriotic supersoldier who often fought the Axis powers of World War II and was Timely Comics\' most popular character during the wartime period. The popularity of superheroes waned following the war and the Captain America comic book was discontinued in 1950, with a short-lived revival in 1953. Since Marvel Comics revived the character in 1964, Captain America has remained in publication.',
+        likes: 1500
+    },
+    {
+        id: 7,
+        avatar: 'assets/images/heroes/7-thumbnail.jpg',
+        name: 'Hulk',
+        alterEgo: 'Bruce Banner',
+        synopsis: 'The Hulk is a fictional superhero appearing in American comic books published by Marvel Comics. Created by writer Stan Lee and artist Jack Kirby, the character first appeared in the debut issue of The Incredible Hulk (May 1962). In his comic book appearances, the character is both the Hulk, a green-skinned, hulking and muscular humanoid possessing a vast degree of physical strength, and his alter ego Dr Robert Bruce Banner, a physically weak, socially withdrawn, and emotionally reserved physicist, the two existing as independent personalities and resenting of the other.',
+        likes: 1500
+    },
+    {
+        id: 8,
+        avatar: 'assets/images/heroes/8-thumbnail.jpg',
+        name: 'Deadpool',
+        alterEgo: 'Wade Winston Wilson',
+        synopsis: 'Deadpool (Wade Winston Wilson) is a fictional character appearing in American comic books published by Marvel Comics. Created by writer Fabian Nicieza and artist/writer Rob Liefeld, the character first appeared in The New Mutants #98 (cover-dated February 1991). Initially Deadpool was depicted as a supervillain when he made his first appearance in The New Mutants and later in issues of X-Force, but later evolved into his more recognizable antiheroic persona. Deadpool, whose real name is Wade Wilson, is a disfigured and deeply disturbed mercenary and assassin with the superhuman ability of an accelerated healing factor and physical prowess. The character is known as the "Merc with a Mouth" because of his tendency to joke constantly, including his proclivity for breaking the fourth wall, a literary device used by the writers for humorous effect and running gags.',
+        likes: 1500
+    },
+    {
+        id: 9,
+        avatar: 'assets/images/heroes/9-thumbnail.jpg',
+        name: 'Wolverine',
+        alterEgo: 'James Howlett',
+        synopsis: 'Wolverine (birthname: James Howlett[1] colloquial: Logan, Weapon X) is a fictional character appearing in American comic books published by Marvel Comics, mostly in association with the X-Men. He is a mutant who possesses animal-keen senses, enhanced physical capabilities, powerful regenerative ability known as a healing factor, and three retractable claws in each hand. Wolverine has been depicted variously as a member of the X-Men, Alpha Flight, and the Avengers.',
+        likes: 1500
+    }
+];
---
+++ app/api/tags.ts
@@ -0,0 +1,19 @@
+import { Tag } from './../shared/models/tag.model';
+export const Tags: Tag[] = [
+    {
+        id: 1,
+        name: 'Marvel'
+    },
+    {
+        id: 2,
+        name: 'DC'
+    },
+    {
+        id: 3,
+        name: 'Avengers'
+    },
+    {
+        id: 4,
+        name: 'Justice Leage'
+    }
+];
